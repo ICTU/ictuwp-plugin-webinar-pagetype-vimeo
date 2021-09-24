b2228d9915f2de946ad9e0bbaada68fc8c2a0eda
Paul van Buuren <paul@wbvb.nl>
2021-09-24 15:53:12 +0200

    code verbeterd

--- ictuwp-plugin-webinar-pagetype-vimeo.php
+++ ictuwp-plugin-webinar-pagetype-vimeo.php
@@ -49,8 +49,8 @@ add_filter( 'template_include', 'ictuwp_vimeoembed_use_page_template' );
  */
 function ictuwp_vimeoembed_add_page_template( $post_templates ) {
 
-	$post_templates[ ICTUWP_VIMEO_EMBED_TEMPLATE_GENESIS ] = _x( 'Webinar embed vimeo (2016)', "naam template", 'ictuwp-plugin-webinar' );
-	$post_templates[ ICTUWP_VIMEO_EMBED_TEMPLATE ]         = _x( 'Webinar embed vimeo (2020)', "naam template", 'ictuwp-plugin-webinar' );
+	$post_templates[ ICTUWP_VIMEO_EMBED_TEMPLATE_GENESIS ] = _x( 'Webinar embed (gc theme 2016)', "naam template", 'ictuwp-plugin-webinar' );
+	$post_templates[ ICTUWP_VIMEO_EMBED_TEMPLATE ]         = _x( 'Webinar embed (gc theme 2020)', "naam template", 'ictuwp-plugin-webinar' );
 
 	return $post_templates;
 
@@ -99,7 +99,7 @@ function ictuwp_vimeoembed_use_page_template( $page_template ) {
 //========================================================================================================
 
 /**
- * Use the page template when necessary
+ * Have the ACF (advanced custom fields) plugin read the settings from this plugin's acf-json folder
  *
  * @since    1.0.0
  */
@@ -117,6 +117,11 @@ add_filter( 'acf/settings/load_json', 'ictuwp_vimeoembed_add_acf_folder' );
 
 //========================================================================================================
 
+/**
+ * Have the ACF (advanced custom fields) plugin save settings to a json file in this plugin's acf-json folder
+ *
+ * @since    1.0.0
+ */
 function ictuwp_vimeoembed_acf_json_save_point( $path ) {
 
 	// update path
@@ -131,6 +136,93 @@ add_filter( 'acf/settings/save_json', 'ictuwp_vimeoembed_acf_json_save_point' );
 
 //========================================================================================================
 
+/**
+ * Add JavaScript and stylesheets
+ *
+ * @global integer $post Post ID.
+ *
+ * @since    1.0.0
+ */
+function ictuwp_vimeoembed_append_vimeo_scripts() {
 
-// HIERONDER ERFENIS
+	global $post;
+
+	if ( ! is_admin() && $post ) {
+
+		$vimeourl = get_field( 'vimeo_url', $post->ID );
+
+		if ( $vimeourl ):
+			// if a Vimeo webinar URL is added, append the embed scripts
+			wp_enqueue_script( 'vimeo-scripts', 'https://player.vimeo.com/api/player.js', '', '', true );
+
+		endif;
+
+		wp_register_style( 'pagetemplate-webinar', plugin_dir_url( __FILE__ ) . 'public/css/webinar-pagetype.css', array(), '' );
+		wp_enqueue_style( 'pagetemplate-webinar' );
+
+
+
+	}
+}
+
+//========================================================================================================
+
+/**
+ * Use the ACF field to embed a vimeo video object
+ *
+ * @global integer $post Post ID.
+ *
+ * @since    1.0.0
+ */
+function ictuwp_vimeoembed_do_embed() {
+
+	global $post;
+
+	// get the ACF field value
+	$vimeourl = get_field( 'vimeo_url', $post->ID );
+
+	if ( $vimeourl ):
+
+		// for example:https://vimeo.com/live/611413510/embed
+		$vimeo_id     = 0;
+		$vimeo_width  = 600;
+		$vimeo_height = 600;
+		$attrs = array();
+
+		$array_vimeo = explode( '/', $vimeourl );
+		foreach ( $array_vimeo as $item ) {
+			if ( is_numeric( $item ) ) {
+				// retrieve the ID, this should be a number
+				$vimeo_id = $item;
+			}
+		}
+
+//		$extra = '?h=a3cc83fe36&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479';
+		$extra = '?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479';
+
+		$embedurl = 'https://player.vimeo.com/video/' . $vimeo_id . $extra;
+/*
+ * <div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/613626035?h=a3cc83fe36&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="test van paul"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
+ */
+
+		if ( $vimeo_id ) {
+			echo '<p><a href="' . $vimeourl . '">' . _x( 'Link naar vimeo', 'gebruikercentraal' ) . '</a></p>';
+			echo '<div style="padding:56.25% 0 0 0;position:relative;border:0.2rem solid #6E9CA5">';
+			echo '<iframe src="' . $embedurl . '" ';
+			echo 'frameborder="0" ';
+			echo 'allow="autoplay; fullscreen; picture-in-picture" ';
+			echo 'style="position:absolute;top:0;left:0;width:100%;height:100%;" ';
+			echo 'webkitallowfullscreen mozallowfullscreen allowfullscreen';
+			echo '></iframe>';
+			echo '</div>';
+		} else {
+
+		}
+
+	endif;
+
+}
+
+//========================================================================================================
+
 
--- public/page_webinar.php
+++ public/page_webinar.php
@@ -1,71 +1,8 @@
 <?php
 
-add_action( 'genesis_entry_content', 'gc_do_vimeo_embed', 11 );
+add_action( 'genesis_entry_content', 'ictuwp_vimeoembed_do_embed', 11 );
 
-add_action( 'wp_enqueue_scripts', 'gc_append_vimeo_scripts' );
-
-remove_action( 'genesis_after_header', 'genesis_do_nav' );
-
-//========================================================================================================
-
-function gc_append_vimeo_scripts() {
-
-	global $post;
-
-	if ( ! is_admin() ) {
-
-		$vimeourl = get_field( 'vimeo_url', $post->ID );
-
-		if ( $vimeourl ):
-			// if a Vimeo webinar URL is added, append the embed scripts
-			wp_enqueue_script( 'vimeo-scripts', 'https://player.vimeo.com/api/player.js', '', '', true );
-
-		endif;
-	}
-}
-
-//========================================================================================================
-
-function gc_do_vimeo_embed() {
-
-	global $post;
-
-	// get the ACF field value
-	$vimeourl = get_field( 'vimeo_url', $post->ID );
-
-	if ( $vimeourl ):
-
-		// for example:https://vimeo.com/live/611413510/embed
-		$vimeo_id     = 0;
-		$vimeo_width  = 600;
-		$vimeo_height = 600;
-
-		$array_vimeo = explode( '/', $vimeourl );
-		foreach ( $array_vimeo as $item ) {
-			if ( is_numeric( $item ) ) {
-				// retrieve the ID, this should be a number
-				$vimeo_id = $item;
-			}
-		}
-
-
-		if ( $vimeo_id ) {
-			echo '<p><a href="' . $vimeourl . '">' . _x( 'Link naar vimeo', 'gebruikercentraal' ) . '</a></p>';
-			echo '<div style="padding:56.25% 0 0 0;position:relative;border:0.2rem solid #6E9CA5">';
-			echo '<iframe src="https://vimeo.com/event/' . $vimeo_id . '/embed" ';
-			echo 'frameborder="0" ';
-			echo 'allow="autoplay; fullscreen; picture-in-picture" ';
-			echo 'style="position:absolute;top:0;left:0;width:100%;height:100%;" ';
-			echo 'webkitallowfullscreen mozallowfullscreen allowfullscreen';
-			echo '></iframe>';
-			echo '</div>';
-		} else {
-
-		}
-
-	endif;
-
-}
+add_action( 'wp_enqueue_scripts', 'ictuwp_vimeoembed_append_vimeo_scripts' );
 
 //========================================================================================================
 
